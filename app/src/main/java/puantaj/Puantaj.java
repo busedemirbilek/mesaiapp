506d4e872adbe1395156a8ab1d69bb3d26dc7ee0
busedemirbilek <busedbilek@hotmail.com>
2021-03-12 04:26:24 +0300

    ilk commit

---
+++ app/src/main/java/puantaj/Puantaj.java
@@ -0,0 +1,13 @@
+package puantaj;
+
+public class Puantaj {
+    public String gun;
+    public double mesai;
+    public String not;
+    public Puantaj(String gun,double mesai,String not)
+    {
+        this.gun=gun;
+        this.mesai=mesai;
+        this.not=not;
+    }
+}
